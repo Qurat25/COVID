a9f5b80334e758843d14c57060dbaf55f6ac8e58
Qurat25 <quratfatima25@gmail.com>
2023-07-08 19:43:10 +0500

    route added

--- client/src/layouts/Frontend/HotspotsMap/HotspotsMap.js
+++ client/src/layouts/Frontend/HotspotsMap/HotspotsMap.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
 function HotspotsMap() {
+  const [data, setdata] = useState({
+    name: "",
+    age: 0,
+    date: "",
+    programming: "",
+  });
+  // Using useEffect for single rendering
+  useEffect(() => {
+    // Using fetch to fetch the api from
+    // flask server it will be redirected to proxy
+    fetch("http://localhost:5000/data").then((res) =>
+      res.json().then((data) => {
+        // Setting a data from api
+        setdata({
+          name: data.Name,
+          age: data.Age,
+          date: data.Date,
+          programming: data.programming,
+        });
+      })
+    );
+  }, []);
+  console.log(data);
   return (
     <DashboardLayout>
       <div className="leaflet-container">
