693dc01c26d193a6a7e99acb093fbd74a9f98608
Qurat25 <quratfatima25@gmail.com>
2023-08-03 04:06:26 +0500

    all records visualized

--- client/src/layouts/Frontend/HotspotsMap/HotspotsMap.js
+++ client/src/layouts/Frontend/HotspotsMap/HotspotsMap.js
@@ -1,30 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, CircleMarker, Popup } from "react-leaflet";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import axios from "axios";
 
 function HotspotsMap() {
-  const [data, setdata] = useState({
-    name: "",
-    age: 0,
-    date: "",
-    programming: "",
-  });
+  const [data, setData] = useState([]);
+
+  // Function to fetch data from Flask API
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/getAllData");
+      const location_data = response.data;
+      var filterData = [];
+
+      for (const location of location_data) {
+        const latitude = location.latitude;
+        const longitude = location.longitude;
+
+        if (latitude === undefined || longitude === undefined) {
+          console.log("Latitude or longitude is undefined.");
+          // Handle undefined values
+        } else {
+          filterData.push({
+            latitude: latitude,
+            longitude: longitude,
+            name: location.name,
+            _id: location._id.$oid,
+          });
+        }
+      }
+      setData(filterData);
+      // setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   // Using useEffect for single rendering
   useEffect(() => {
-    // Using fetch to fetch the api from
-    // flask server it will be redirected to proxy
-    fetch("http://localhost:5000/data").then((res) =>
-      res.json().then((data) => {
-        // Setting a data from api
-        setdata({
-          name: data.Name,
-          age: data.Age,
-          date: data.Date,
-          programming: data.programming,
-        });
-      })
-    );
+    fetchData();
   }, []);
+
   console.log(data);
   return (
     <DashboardLayout>
@@ -34,6 +50,21 @@ function HotspotsMap() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
+          {data.length > 0 &&
+            data.map((item, index) => (
+              <CircleMarker
+                key={item._id}
+                center={[item.latitude, item.longitude]}
+                radius={15}
+                pathOptions={{ color: "red" }}
+              >
+                <Popup>
+                  <div>
+                    <strong>{item.name}</strong>
+                  </div>
+                </Popup>
+              </CircleMarker>
+            ))}
         </MapContainer>
       </div>
     </DashboardLayout>
--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+from bson import json_util
 
 # Import configuration values
 from config import DEBUG, MONGO_URI, MONGO_DB
@@ -10,11 +11,17 @@ app = Flask(__name__)
 app.config['DEBUG'] = DEBUG
 
 # Configure MongoDB
-app.config['MONGO_URI'] = MONGO_URI
-app.config['MONGO_DBNAME'] = MONGO_DB
+app.config['MONGO_URI']='mongodb://localhost:27017/covid'
 mongo = PyMongo(app)
 
 CORS(app)
+
+@app.route('/api/getAllData', methods=['GET'])
+def get_all_data():
+    collection = mongo.db.locations # Replace 'collection_name' with the name of your collection
+    cursor = collection.find()
+    data = json_util.dumps(list(cursor))  # Use json_util to serialize MongoDB data types
+    return data
 	
 # Running app
 if __name__ == '__main__':
